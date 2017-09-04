c2b1004d908936eaa67220bd8c1aa4746fb9f2de
adithyahosapate <adihosapate@gmail.com>
2017-09-04 22:21:50 +0530

    updated svm

--- svm.py
+++ svm.py
@@ -1,3 +1,18 @@
 import cvxopt
+import numpy as np
+from Kernel import kernel
 
 # TODO - optimize svms using Quadratic programming
+class SVM:
+	def __init__(self,dataset,labels,kernel):
+		self.dataset=dataset
+		self.labels=labels
+		self.kernel=kernel
+
+	def optimize(self):
+		def create_gram_matrix(self):
+			self.gram_matrix=np.zeroes(self.dataset.shape[0])
+			for i,data in enumerate(dataset):
+				for (i,data_2) in enumerate(dataset):
+					gram_matrix[i][j]=kernel(data,data_2)
+			return self.gram_matrix		
