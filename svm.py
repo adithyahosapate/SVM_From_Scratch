4552a27399e215caff27858380e66776c939ba3e
adithyahosapate <adihosapate@gmail.com>
2017-09-05 16:41:55 +0530

    added quadratic form

--- svm.py
+++ svm.py
@@ -1,6 +1,6 @@
 import cvxopt
 import numpy as np
-from Kernel import kernel
+from kernels import Kernel
 
 # TODO - optimize svms using Quadratic programming
 class SVM:
@@ -10,9 +10,18 @@ class SVM:
 		self.kernel=kernel
 
 	def optimize(self):
-		def create_gram_matrix(self):
-			self.gram_matrix=np.zeroes(self.dataset.shape[0])
-			for i,data in enumerate(dataset):
-				for (i,data_2) in enumerate(dataset):
-					gram_matrix[i][j]=kernel(data,data_2)
-			return self.gram_matrix		
+		def create_gram_matrix():
+			gram_matrix=np.zeros([self.dataset.shape[0],self.dataset.shape[0]])
+			for i,data in enumerate(self.dataset):
+				for (j,data_2) in enumerate(self.dataset):
+					gram_matrix[i][j]=self.labels[i]*self.labels[j]*np.inner(data,data_2)
+			return gram_matrix
+		self.gram_matrix=create_gram_matrix()			
+		print(cvxopt.solvers.qp(P=-self.gram_matrix,
+						q=np.ones([self.dataset.shape[0],1]),
+						G=-np.eye(self.dataset.shape[0]),
+						h=np.zeros([self.dataset.shape[0],1]),
+						A=np.diag(self.labels),
+						b=np.zeros([self.dataset.shape[0],1]),
+						solver=None))
+
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from svm_sgd import SVM
+from svm import SVM
 import numpy as np
 import matplotlib.pyplot as plt
 X1=np.random.multivariate_normal([1,2],[[1,0],[0,1]],size=100)
@@ -6,7 +6,7 @@ Y=np.random.choice([1],100)
 X2=np.random.multivariate_normal([-10,5],[[1,0],[0,1]],size=100)
 X=np.concatenate((X1,X2),axis=0)
 Y=np.concatenate((Y,-Y))
-svm=SVM(X,Y)
-w,b=svm.classify(1000,0.01,"linear")
+svm=SVM(X,Y,"linear")
+w,b=svm.optimize()
 print(w,b)
 svm.plot()
