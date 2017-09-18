e58c8b8aed06579833080b8bc0c44b78e74e7c2c
adithyahosapate <adihosapate@gmail.com>
2017-09-18 21:31:33 +0530

    removed notebook

--- test.py
+++ test.py
@@ -12,19 +12,3 @@ svm.plot()
 
 #Test 2
 
-points_array=np.array([
-	[1,2],
-	[1.2,3.5],
-	[1,7],
-	[1.7,5.6],
-	[5,7],
-	[3.0,4.7],
-	[5,8],
-	[-1,1],
-	[1,7]
-
-	])
-labels_array=np.array([-1,1,1,-1,1,-1,1,1,1])
-svm=SVM(points_array,labels_array,"gaussian")
-alphas=svm.optimize()
-svm.plot()
